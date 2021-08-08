8947f4714e61ae61e7ff1c93cd746665e9da4c16
Abhay <abhaykvincent@gmail.com>
2021-08-08 17:21:50 -0400

    Markup basic structure create

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.scss'
 function App() {
   return (
-    <div className="App">
-      Mirza
+    <div className="Mirza">
+      <header className="header">Header</header>
+      <section className="hero">hero</section>
+      <section className="feature-images">feature-images</section>
+      <section className="fashion-meets-design-i">ashion-meets-design-i</section>
+      <section className="fashion">ashion</section>
+      <section className="design">design</section>
+      <section className="fashion-meets-design-ii">fashion-meets-design-ii</section>
+      <section className="bio-brief">bio-brief</section>
+      <section className="mirza-gallery">mirza-gallery</section>
+      <section className="clients">clients</section>
+      <section className="cta">cta</section>
+      <div className="footer">Footer</div>
     </div>
   );
 }
