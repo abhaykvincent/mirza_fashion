e020a2cc1a2f7f3d70db1dfb0bd34e96e92e3243
Abhay <abhaykvincent@gmail.com>
2021-08-10 09:29:24 -0400

    clients updated

--- src/App.js
+++ src/App.js
@@ -85,10 +85,7 @@ function App() {
           </div>
         </div>
       </section>
-      <section className="clients border">
-        <div className="border left">Left</div>
-        <div className="border right">Left</div>
-      </section>
+      
       <section className="cta">
         <div className="cta-content">
           <div className="cta-title"></div>
